6d3cf921fe7ffa9918f319b1f526143f714664a3
AartiJakhar <aartijakharkct@gmail.com>
2023-01-04 15:41:47 +0530

    fixing bug

--- midleware/mongoose.ts
+++ midleware/mongoose.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-const connectDb= handler => async(req,res)=>{
+const connectDb= (handler:  any )  => async(req:any,res:any)=>{
     if(mongoose.connections[0].readyState){
          return handler(req,res)
     }
-    await mongoose.connect(process.env.MONGO_URI as any)
+    await mongoose.connect(process.env.MONGODB_URI as any)
     return handler(req,res);
 }
 export default connectDb;
