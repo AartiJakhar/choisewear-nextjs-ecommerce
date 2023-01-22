f9fe98a1d86d870389fb7ab324836381045dd973
AartiJakhar <aartijakharkct@gmail.com>
2023-01-22 10:08:33 +0530

    adding responsiveness in banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,13 +5,13 @@ export default function Banner() {
 
   return (
 
-    <div className={` bg-[url('/banner.svg')] lg:bg-cover  bg-contain bg-no-repeat h-[30rem] `}>
+    <div className={` bg-[url('/banner.png')] lg:bg-cover  bg-contain bg-no-repeat md:h-[30rem] sm:h-[25rem] h-[20rem]  `}>
       <div className={`md:w-[28rem] w-[21rem] sm:justify-center   pl-8 pt-8 md:pl-16  lg:pl-28 md:pt-20 flex justify-start  flex-col `}>
         <h1 className=' md:text-3xl sm:text-2xl text-xl font-bold font-serif   '>
           Welcome to <span className='block  md:text-4xl sm:text-3xl text-2xl text-pink-800 lg:text-5xl '>CHOISEWEAR</span>
           <div className='border-solid border-b-2 border-x-lime-900 md:w-72 sm:w-60  w-40'></div>
         </h1>
-        <p className='text-sm font-serif sm:pt-6 md:text-2xl sm:w-full w-52'>
+        <div className='text-sm font-serif sm:pt-6 md:text-2xl sm:w-full w-52'>
         Here you can buy best designed or quaility - 
         <Typewriter
   options={{
@@ -19,7 +19,8 @@ export default function Banner() {
     autoStart: true,
     loop: true,
   }}
-/> </p>
+/>
+ </div>
       </div>
        
     
--- components/Content.tsx
+++ components/Content.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Content() {
   return (
 <section className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto">
+  <div className="container px-5 pb-24 md:pt-12 mx-auto">
     <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
       <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
