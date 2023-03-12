425d96bbd38ef6d3c8f83cfd546fe2086054d45a
AartiJakhar <aartijakharkct@gmail.com>
2023-03-12 20:48:49 +0530

    fixing bug

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -35,21 +35,7 @@ export default function Navbar({ logOut, user, addToCart, cart, removeFromCart, 
    
          if (typeof window !== "undefined") {
            window.addEventListener('scroll',changeBackground)
-           
-           window.onclick = function (event) {
-               if (!event.target.matches('.dropbtn') ) {
-                   var dropdowns = document.getElementsByClassName(styles.nav);
-                   var i;
-                   for (i = 0; i < dropdowns.length; i++) { 
-                       var openDropdown = dropdowns[i];
-                       if (openDropdown.classList.contains(`${styles.toggle}`)) {
-                           console.log('first window')
-                           openDropdown.classList.remove(`${styles.toggle}`);
-                           setdropdownToggle(false)
-                       }
-                   }
-               }
-           }
+         
        }
        const [sidebarToggler, setSidebarToggler] = useState(true)
        let dataList =[
